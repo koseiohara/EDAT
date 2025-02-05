e94114f093fe20ffec39c167c8cca5539c5f0c04
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2025-02-06 02:05:20 +0900

    Rename of Modules

--- src/edat_math.f90
+++ src/edat_math.f90
@@ -1,11 +1,27 @@
-module EDAT_Statistic
+module EDAT_Math
 
     implicit none
 
     private :: rk
-    public :: qnorm, corrcoef, covariance, variance, mean, sum_hp
+    public :: M_E, M_LOG2E, M_LOG10E, M_LN2, M_LN10, M_PI, M_PI_2, M_PI_4, M_1_PI, M_2_PI, M_2_SQRTPI, M_SQRT2, M_SQRT1_2, &
+            & qnorm, corrcoef, covariance, variance, mean, sum_hp
 
-    integer, parameter :: rk = 8
+    integer , parameter :: rk = 16
+
+    real(rk), parameter :: M_E        = 2.718281828459045235360287471352662498_rk       ! e
+    real(rk), parameter :: M_LOG2E    = 1.442695040888963407359924681001892137_rk       ! log_2 e
+    real(rk), parameter :: M_LOG10E   = 0.434294481903251827651128918916605082_rk       ! log_10 e
+    real(rk), parameter :: M_LN2      = 0.693147180559945309417232121458176568_rk       ! log_e 2
+    real(rk), parameter :: M_LN10     = 2.302585092994045684017991454684364208_rk       ! log_e 10
+    real(rk), parameter :: M_PI       = 3.141592653589793238462643383279502884_rk       ! pi
+    real(rk), parameter :: M_PI_2     = 1.570796326794896619231321691639751442_rk       ! pi/2
+    real(rk), parameter :: M_PI_4     = 0.785398163397448309615660845819875721_rk       ! pi/4
+    real(rk), parameter :: M_1_PI     = 0.318309886183790671537767526745028724_rk       ! 1/pi
+    real(rk), parameter :: M_2_PI     = 0.636619772367581343075535053490057448_rk       ! 2/pi
+    real(rk), parameter :: M_2_SQRTPI = 1.128379167095512573896158903121545172_rk       ! 2/sqrt(pi)
+    real(rk), parameter :: M_SQRT2    = 1.414213562373095048801688724209698079_rk       ! sqrt(2)
+    real(rk), parameter :: M_SQRT1_2  = 0.707106781186547524400844362104849039_rk       ! 1/sqrt(2)
+
     real(rk), parameter :: qnorm(77)=[0.0125334695_rk, &  !! 01
                                     & 0.0250689082_rk, &  !! 02
                                     & 0.0376082876_rk, &  !! 03
@@ -465,5 +481,5 @@ module EDAT_Statistic
     end function sum_hp_qp
 
 
-end module EDAT_Statistic
+end module EDAT_Math
 
--- src/edat_met.f90
+++ src/edat_met.f90
@@ -1,23 +1,10 @@
-module EDAT_Physics
+module EDAT_Met
 
     implicit none
 
     private :: rk
 
     integer , parameter :: rk = 16
-    real(rk), parameter :: P_E        = 2.718281828459045235360287471352662498_rk       ! e
-    real(rk), parameter :: P_LOG2E    = 1.442695040888963407359924681001892137_rk       ! log_2 e
-    real(rk), parameter :: P_LOG10E   = 0.434294481903251827651128918916605082_rk       ! log_10 e
-    real(rk), parameter :: P_LN2      = 0.693147180559945309417232121458176568_rk       ! log_e 2
-    real(rk), parameter :: P_LN10     = 2.302585092994045684017991454684364208_rk       ! log_e 10
-    real(rk), parameter :: P_PI       = 3.141592653589793238462643383279502884_rk       ! pi
-    real(rk), parameter :: P_PI_2     = 1.570796326794896619231321691639751442_rk       ! pi/2
-    real(rk), parameter :: P_PI_4     = 0.785398163397448309615660845819875721_rk       ! pi/4
-    real(rk), parameter :: P_1_PI     = 0.318309886183790671537767526745028724_rk       ! 1/pi
-    real(rk), parameter :: P_2_PI     = 0.636619772367581343075535053490057448_rk       ! 2/pi
-    real(rk), parameter :: P_2_SQRTPI = 1.128379167095512573896158903121545172_rk       ! 2/sqrt(pi)
-    real(rk), parameter :: P_SQRT2    = 1.414213562373095048801688724209698079_rk       ! sqrt(2)
-    real(rk), parameter :: P_SQRT1_2  = 0.707106781186547524400844362104849039_rk       ! 1/sqrt(2)
 
     real(rk), parameter :: GRAV        = 9.80665_rk         ! Gravitational Acceleration [m/s^2]
     real(rk), parameter :: EarthRadius = 6.3710E+6_rk       ! Radius of the Earth [m]
@@ -77,6 +64,6 @@ module EDAT_Physics
     end function potential_temperature_qp
 
 
-end module EDAT_Physics
+end module EDAT_Met
 
 

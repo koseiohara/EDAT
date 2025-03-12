8b5d4def1691612356686c369f217e5a1fbcd4f3
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2025-03-12 18:41:55 +0900

    New files : c_qsort.c and edat_sort.f90
    edat_sort.f90 is a wrapper module of qsort() of C-lang

---
+++ src/c_qsort.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+
+
+int compare_i4(const void* a, const void* b){
+    int a_cp = *(const int*)a;
+    int b_cp = *(const int*)b;
+
+    if (a_cp < b_cp){
+        return -1;
+    }else if (a_cp > b_cp){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+int compare_sp(const void* a, const void* b){
+    float a_cp = *(const float*)a;
+    float b_cp = *(const float*)b;
+
+    if (a_cp < b_cp){
+        return -1;
+    }else if (a_cp > b_cp){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+int compare_dp(const void* a, const void* b){
+    double a_cp = *(const double*)a;
+    double b_cp = *(const double*)b;
+
+    if (a_cp < b_cp){
+        return -1;
+    }else if (a_cp > b_cp){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+
+void c_quick_sort_i4(const int* n, int array[]){
+    qsort(array, *n, sizeof(int), compare_i4);
+}
+
+
+void c_quick_sort_sp(const int* n, float array[]){
+    qsort(array, *n, sizeof(float), compare_sp);
+}
+
+
+void c_quick_sort_dp(const int* n, double array[]){
+    qsort(array, *n, sizeof(double), compare_dp);
+}
+
---
+++ src/edat_sort.f90
@@ -0,0 +1,76 @@
+module EDAT_Sort
+    use iso_c_binding
+
+    implicit none
+
+    private
+    public :: quick_sort
+
+    interface quick_sort
+        module procedure &
+            & quick_sort_i4, &
+            & quick_sort_sp, &
+            & quick_sort_dp
+    end interface quick_sort
+
+    interface
+        subroutine c_quick_sort_i4(n, array) bind(C)
+            use iso_c_binding
+            integer(c_int), intent(in)    :: n
+            integer(c_int), intent(inout) :: array(n)
+        end subroutine c_quick_sort_i4
+    end interface
+
+
+    interface
+        subroutine c_quick_sort_sp(n, array) bind(C)
+            use iso_c_binding
+            integer(c_int), intent(in)    :: n
+            real(c_float) , intent(inout) :: array(n)
+        end subroutine c_quick_sort_sp
+    end interface
+
+
+    interface
+        subroutine c_quick_sort_dp(n, array) bind(C)
+            use iso_c_binding
+            integer(c_int), intent(in)    :: n
+            real(c_double), intent(inout) :: array(n)
+        end subroutine c_quick_sort_dp
+    end interface
+
+
+    contains
+
+
+    subroutine quick_sort_i4(n, array)
+        integer, intent(in)    :: n
+        integer, intent(inout) :: array(n)
+
+        call c_quick_sort_i4(n, array(1:n))
+
+    end subroutine quick_sort_i4
+
+
+    subroutine quick_sort_sp(n, array)
+        integer , parameter :: rk = 4
+        integer , intent(in)    :: n
+        real(rk), intent(inout) :: array(n)
+
+        call c_quick_sort_sp(n, array(1:n))
+
+    end subroutine quick_sort_sp
+
+
+    subroutine quick_sort_dp(n, array)
+        integer , parameter :: rk = 8
+        integer , intent(in)    :: n
+        real(rk), intent(inout) :: array(n)
+
+        call c_quick_sort_dp(n, array(1:n))
+
+    end subroutine quick_sort_dp
+
+
+end module EDAT_Sort
+

36daaa95804d863088f3eeb5e08dc22a0fc243f6
kosei <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-07-19 07:07:08 +0900

    auttools build was successed

--- sample_build.sh
+++ sample_build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-./configure --prefix=$HOME/FortranLib \
-            FC="ifort" \
-            CC="icc" \
-            FCFLAGS="-O3 -traceback -warn all -convert little_endian -assume byterecl" \
-            CFLAGS="-O3 -Wall"
+./configure --prefix=$HOME/FortranLib FC="ifort" CC="icc" FCFLAGS="-O3 -traceback -warn all -convert little_endian -assume byterecl" CFLAGS="-O3 -Wall"
 

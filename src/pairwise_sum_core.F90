81a4440850344af44959d3015c34d0162825c0f5
koseiohara <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-09-15 17:34:50 +0900

    Support arbitrary-length pairwise reductions

--- src/pairwise_sum_core.F90
+++ src/pairwise_sum_core.F90
@@ -1,5 +1,111 @@
 
+pure function largest_power_of_2(n) result(output)
+    use, intrinsic :: iso_fortran_env, only : ik=>int64
+    integer(ik), intent(in) :: n
+    integer(ik) :: output
+    integer(ik) :: work
+
+    work   = n
+    output = 1_ik
+    do
+        if (work <= 1_ik) then
+            exit
+        endif
+        work = shiftr(work, 1)
+        output = output + output
+    enddo
+
+end function largest_power_of_2
+
+
+pure subroutine RESIZE(n, n_resized, stride, howmany, arr)
+    use, intrinsic :: iso_fortran_env, only : ik=>int64
+
+    integer(ik), intent(in) :: n
+    integer(ik), intent(in) :: n_resized
+    integer(ik), intent(in) :: stride
+    integer(ik), intent(in) :: howmany
+    real(RK)   , intent(inout), contiguous :: arr(:)
+
+    integer(ik) :: remainder_n
+    integer(ik) :: half_n
+    integer(ik) :: resized_group_n
+    integer(ik) :: old_offset
+    integer(ik) :: new_offset
+    integer(ik) :: source_offset
+    integer(ik) :: target_offset
+    integer(ik) :: dist
+    integer(ik) :: position
+    integer(ik) :: position_remainder
+    integer(ik) :: position_step
+    integer(ik) :: position_step_remainder
+    integer(ik) :: group
+    integer(ik) :: tail
+    integer(ik) :: i
+
+    if (n == n_resized) then
+        return
+    endif
+
+    remainder_n     = n - n_resized
+    half_n          = shiftr(n_resized, 1)
+    resized_group_n = n_resized * stride
+
+    do group = 0_ik, howmany-1_ik
+        old_offset = group * n * stride
+        new_offset = group * resized_group_n
+
+        if (new_offset /= old_offset) then
+            do i = 1_ik, resized_group_n
+                arr(new_offset+i) = arr(old_offset+i)
+            enddo
+        endif
+
+        if (remainder_n+remainder_n-1_ik <= huge(0_ik) / half_n) then
+            do tail = 1_ik, remainder_n
+                dist = 1_ik + (tail+tail-1_ik) * half_n / remainder_n
+                source_offset = old_offset + (n_resized+tail-1_ik) * stride
+                target_offset = new_offset + (dist-1_ik) * stride
+
+                arr(target_offset+1_ik:target_offset+stride) = arr(target_offset+1_ik:target_offset+stride) + &
+                                                               arr(source_offset+1_ik:source_offset+stride)
+            enddo
+        else
+            position                = half_n / remainder_n
+            position_remainder      = modulo(half_n, remainder_n)
+            position_step           = n_resized / remainder_n
+            position_step_remainder = modulo(n_resized, remainder_n)
+
+            do tail = 1_ik, remainder_n
+                dist = 1_ik + position
+                source_offset = old_offset + (n_resized+tail-1_ik) * stride
+                target_offset = new_offset + (dist-1_ik) * stride
+
+                arr(target_offset+1_ik:target_offset+stride) = arr(target_offset+1_ik:target_offset+stride) + &
+                                                               arr(source_offset+1_ik:source_offset+stride)
+
+                if (tail < remainder_n) then
+                    position           = position + position_step
+                    position_remainder = position_remainder + position_step_remainder
+
+                    if (position_remainder >= remainder_n) then
+                        position_remainder = position_remainder - remainder_n
+                        position = position + 1_ik
+                    endif
+                endif
+            enddo
+        endif
+    enddo
+
+end subroutine RESIZE
+
+
+#ifdef DEBUG
 subroutine PROC(n, howmany, stride, iarr, oarr)
+#else
+pure subroutine PROC(n, howmany, stride, iarr, oarr)
+#endif
+
     use, intrinsic :: iso_fortran_env, only : ik=>int64
 #ifdef DEBUG
     use, intrinsic :: iso_fortran_env, only : ounit=>output_unit
@@ -12,6 +118,7 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
     real(RK)   , intent(out)  , contiguous :: oarr(:)
 
     real(RK), allocatable :: work_arr(:)
+    integer(ik) :: n_resized
     integer(ik) :: nwork
     integer(ik) :: work_n
     integer(ik) :: half_n
@@ -24,7 +131,16 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
     integer(ik) :: idx1
     integer(ik) :: idx2
 
-    work_n = n
+    n_resized = largest_power_of_2(n)
+
+    call RESIZE(n        , &  !! IN
+              & n_resized, &  !! IN
+              & stride   , &  !! IN
+              & howmany  , &  !! IN
+              & iarr(:)    )  !! INOUT
+
+
+    work_n = n_resized
     half_n = shiftr(work_n, 1)
     nwork  = half_n * howmany * stride
 
@@ -37,8 +153,8 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
 #ifdef DEBUG
                 write(ounit,'(A,I0)') 'i = ', i
 #endif
-                skip_iarr = i *      n * stride
-                skip_work = i * half_n * stride
+                skip_iarr = i * n_resized * stride
+                skip_work = i * half_n    * stride
 
                 idx1 = 1
                 idx2 = 1 + stride
@@ -68,8 +184,8 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
 #ifdef DEBUG
                 write(ounit,'(A,I0)') 'i = ', i
 #endif
-                skip_iarr = i *      n * stride
-                skip_work = i * half_n * stride
+                skip_iarr = i * n_resized * stride
+                skip_work = i * half_n    * stride
 
                 idx1 = 1
                 idx2 = 1 + stride
@@ -102,7 +218,7 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
 
     if (work_n == 2) then
         do i = 0, howmany-1
-            skip_iarr = i * n * stride
+            skip_iarr = i * n_resized * stride
             skip_work = i * stride
             do j = 1, stride
 #ifdef DEBUG
@@ -115,7 +231,7 @@ subroutine PROC(n, howmany, stride, iarr, oarr)
         enddo
     else
         do i = 0, howmany-1
-            skip_iarr = i * n * stride
+            skip_iarr = i * n_resized * stride
             skip_work = i * stride
             do j = 1, stride
 #ifdef DEBUG

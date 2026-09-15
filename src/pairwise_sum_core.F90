819b4d7288e2e2305adb4af3871fcfa60fc11b4a
koseiohara <ohara.kosei.s3@dc.tohoku.ac.jp>
2026-09-15 14:12:23 +0900

    add: implement grouped strided pairwise summation core

---
+++ src/pairwise_sum_core.F90
@@ -0,0 +1,120 @@
+
+subroutine PROC(n, howmany, stride, iarr, oarr)
+    use, intrinsic :: iso_fortran_env, only : ik=>int64
+#ifdef DEBUG
+    use, intrinsic :: iso_fortran_env, only : ounit=>output_unit
+#endif
+
+    integer(ik), intent(in) :: n
+    integer(ik), intent(in) :: howmany
+    integer(ik), intent(in) :: stride
+    real(RK)   , intent(inout), contiguous :: iarr(:)
+    real(RK)   , intent(out)  , contiguous :: oarr(:)
+
+    real(RK), allocatable :: work_arr(:)
+    integer(ik) :: nwork
+    integer(ik) :: work_n
+    integer(ik) :: half_n
+    integer(ik) :: i
+    integer(ik) :: j
+    integer(ik) :: k
+    integer(ik) :: skip_iarr
+    integer(ik) :: skip_work
+    integer(ik) :: idxl
+    integer(ik) :: idx1
+    integer(ik) :: idx2
+
+    work_n = n
+    half_n = shiftr(work_n, 1)
+    nwork  = half_n * howmany * stride
+
+    allocate(work_arr(nwork))
+
+    do
+        if (work_n > 2) then
+            work_n = shiftr(work_n, 1)
+            do i = 0, howmany-1
+#ifdef DEBUG
+                write(ounit,'(A,I0)') 'i = ', i
+#endif
+                skip_iarr = i *      n * stride
+                skip_work = i * half_n * stride
+
+                idx1 = 1
+                idx2 = 1 + stride
+                do j = 1, work_n
+                    idxl = (j - 1) * stride
+                    do k = 1, stride
+#ifdef DEBUG
+                        write(ounit,'(A,I0,A,I0,A,I0,A)') 'work_arr(', skip_work+k+idxl, &
+                                                        & ') = iarr(', skip_iarr+idx1, &
+                                                        & ') + iarr(', skip_iarr+idx2, ')'
+#endif
+                        work_arr(skip_work+k+idxl) = iarr(skip_iarr+idx1) + iarr(skip_iarr+idx2)
+
+                        idx1 = idx1 + 1
+                        idx2 = idx1 + stride
+                    enddo
+                    idx1 = idx2
+                    idx2 = idx1 + stride
+                enddo
+            enddo
+
+            work_n = shiftr(work_n, 1)
+            do i = 0, howmany-1
+#ifdef DEBUG
+                write(ounit,'(A,I0)') 'i = ', i
+#endif
+                skip_iarr = i *      n * stride
+                skip_work = i * half_n * stride
+
+                idx1 = 1
+                idx2 = 1 + stride
+                do j = 1, work_n
+                    idxl = (j - 1) * stride
+                    do k = 1, stride
+#ifdef DEBUG
+                        write(ounit,'(A,I0,A,I0,A,I0,A)') 'iarr(', skip_iarr+k+idxl, &
+                                                        & ') = work_arr(', skip_work+idx1,&
+                                                        & ') + work_arr(', skip_work+idx2, ')'
+#endif
+                        iarr(skip_iarr+k+idxl) = work_arr(skip_work+idx1) + work_arr(skip_work+idx2)
+
+                        idx1 = idx1 + 1
+                        idx2 = idx1 + stride
+                    enddo
+                    idx1 = idx2
+                    idx2 = idx1 + stride
+                enddo
+            enddo
+
+            cycle
+        else
+            exit
+        endif
+    enddo
+
+    if (work_n == 2) then
+        do i = 0, howmany-1
+            skip_iarr = i * n * stride
+            skip_work = i * stride
+            do j = 1, stride
+#ifdef DEBUG
+            write(ounit,'(A,I0,A,I0,A,I0,A)') 'oarr(', skip_work+j, ') = iarr(', skip_iarr+j, ') + iarr(', skip_iarr+j+stride, ')'
+#endif
+                oarr(skip_work+j) = iarr(skip_iarr+j) + iarr(skip_iarr+j+stride)
+            enddo
+        enddo
+    else
+        do i = 0, howmany-1
+            skip_iarr = i * n * stride
+            skip_work = i * stride
+            do j = 1, stride
+#ifdef DEBUG
+            write(ounit,'(A,I0,A,I0,A,I0,A)') 'oarr(', skip_work+j, ') = iarr(', skip_iarr+j, ')'
+#endif
+                oarr(skip_work+j) = iarr(skip_iarr+j)
+            enddo
+        enddo
+    endif
+end subroutine PROC
